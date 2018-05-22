70bdbbf4f3db1561d0bd609edace97bce613ba26
w-sgh <w.seghouane@gmail.com>
2018-05-22 16:22:32 +0200

    projet3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1EnfantComponent implements OnInit {
 
+  @Input() public compteur: number;
+  @Output() public result = new EventEmitter;
+
+
   constructor() { }
+
+  increment() {
+    this.compteur ++;
+    this.result.emit({
+      value: this.compteur
+    });
+  }
+
+  decrement() {
+    this.compteur --;
+    this.result.emit({
+      value: this.compteur
+    });
+  }
 
   ngOnInit() {
   }
